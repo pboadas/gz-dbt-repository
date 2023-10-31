4f2be05a3b27154cbea37ea1801515d0e26db294
pierreboadas@gmail.com <pierreboadas@gmail.com>
2023-10-31 14:40:45 +0000

    JOIN + margin + purchase cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,9 @@
+SELECT quantity
+, revenue
+, product_id
+, purchase_price
+, cast(purchase_price as FLOAT64)*quantity as purchase_cost
+, revenue-(cast(purchase_price as FLOAT64)*quantity) as margin
+FROM {{ref("stg_raw__sales")}} as sales
+JOIN {{ref("stg_raw__product")}} as product
+on sales.product_id=product.products_id
